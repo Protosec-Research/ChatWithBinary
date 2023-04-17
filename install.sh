8d8f1d5b26ec0f957e79cf2fa45bc02169788d24
DDizzzy79 <patrick_0429@163.com>
2023-04-17 17:08:58 +0800

    Fix bugs

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 python_path=$(which python3)
 echo "#!$python_path" > temp_file.txt
-cat ret2gpt.py >> temp_file.txt
-mv temp_file.txt ret2gpt.py
+cat Ret2GPT.py >> temp_file.txt
+mv temp_file.txt Ret2GPT.py
 
 
 python3 -m pip install -r requirements.txt
-ln -s "$(pwd)/ret2gpt.py" /usr/local/bin/ret2gpt
+ln -s "$(pwd)/Ret2GPT.py" /usr/local/bin/ret2gpt
 chmod +x /usr/local/bin/ret2gpt
 echo "Installation completed successfully."
