e55d7fc1b6911400d8d2787f8ee3c20d5f014d06
DDizzzy79 <patrick_0429@163.com>
2023-04-18 20:51:46 +0800

    Fixed bug

--- Ret2GPT.py
+++ Ret2GPT.py
@@ -1,6 +1,3 @@
-#!/opt/homebrew/bin/python3
-#!/opt/homebrew/bin/python3
-#!/opt/homebrew/bin/python3
 from langchain_preprocess import prompt_builder as pwnchain
 from langchain.embeddings import OpenAIEmbeddings
 from built_in_command import command_line
